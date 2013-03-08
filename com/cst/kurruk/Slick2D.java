fa0f30e91a2b8411a251558c8131e0456aee19c1
cstcodingTeam <cstcodingteam@gmail.com>
2013-03-08 14:26:59 -0500

    Almost got key movement working

--- com/cst/kurruk/Slick2D.java
+++ com/cst/kurruk/Slick2D.java
@@ -14,21 +14,25 @@ public class Slick2D extends BasicGame {
 	@Override
 	public void init(GameContainer gamecontainer) throws SlickException {
 		background = new Image("background.jpg");
-		player = new Player(new Image("player.jpg"));
+		player = new Player(new Image("player.jpg"), 236, 160);
 	}
 	
 	@Override
 	public void update(GameContainer gamecontainer, int i) throws SlickException {
+		player.update();
 	}
 	
 	@Override
 	public void render(GameContainer gamecontainer, Graphics graphics) throws SlickException {
 		background.draw();
+		player.render();
 	}
 	
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new Slick2D());
 		
+		app.setMinimumLogicUpdateInterval(5);
+		app.setMaximumLogicUpdateInterval(5);
 		app.setDisplayMode(1280, 720, false);
 		app.setShowFPS(false);
 		app.start();
